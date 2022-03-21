fc2d31834c448482f0502653014099a6cf3072e6
Dasha Zaets <daryaazaets@gmail.com>
2022-03-21 13:16:49 +0100

    done game

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
 const quotes = [
-    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
-    'The fool doth think he is wise, but the wise man knows himself to be a fool.',
-    'Love all, trust a few, do wrong to none.',
-    'Hell is empty and all the devils are here.',
-    'Do not waste your love on somebody, who does not value it',
-    'There is nothing more deceptive than an obvious fact.',
-    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
-    'I never make exceptions. An exception disproves the rule.',
-    'What one man can invent another can discover.',
-    'Nothing clears up a case so much as stating it to another person.',
-    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+  'When you have eliminated the impossible, whatever remains, however improbable, must be the truth',
+  'The fool doth think he is wise, but the wise man knows himself to be a fool',
+  'Love all, trust a few, do wrong to none',
+  'Hell is empty and all the devils are here',
+  'Do not waste your love on somebody, who does not value it',
+  'There is nothing more deceptive than an obvious fact',
+  'I never make exceptions. An exception disproves the rule',
+  'What one man can invent another can discover',
+  'Nothing clears up a case so much as stating it to another person',
+  'Education never ends, Watson. It is a series of lessons, with the greatest for the last',
 ];
 // store the list of words and the index of the word the player is currently typing
 let words = [];
@@ -21,68 +20,99 @@ let startTime = Date.now();
 const quoteElement = document.getElementById('quote');
 const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-value');
+const startButtonElement = document.getElementById('start');
+const aboutGameElement = document.getElementById('about')
+const clickToGameElement = document.getElementById('click-to')
+const carElement = document.getElementById('car-image')
+const inputElement = document.getElementById('input')
 
-document.getElementById('start').addEventListener('click', () => {
-    // get a quote
-    const quoteIndex = Math.floor(Math.random() * quotes.length);
-    const quote = quotes[quoteIndex];
-    // Put the quote into an array of words
-    words = quote.split(' ');
-    // reset the word index for tracking
-    wordIndex = 0;
-  
-    // UI updates
-    // Create an array of span elements so we can set a class
-    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
-    // Convert into string and set as innerHTML on quote display
-    quoteElement.innerHTML = spanWords.join('');
-    // Highlight the first word
-    quoteElement.childNodes[0].className = 'highlight';
-    // Clear any prior messages
-    messageElement.innerText = '';
-  
-    // Setup the textbox
-    // Clear the textbox
-    typedValueElement.value = '';
-    // set focus
-    typedValueElement.focus();
-    // set the event handler
-  
-    // Start the timer
-    startTime = new Date().getTime();
-  });
+inputElement.classList.add("hidden")
 
-  // at the end of script.js
+startButtonElement.addEventListener('click', () => {
+  startButtonElement.classList.add("hidden")
+  aboutGameElement.classList.add("hidden")
+  clickToGameElement.classList.add('hidden')    
+  quoteElement.classList.remove("hidden")
+  typedValueElement.classList.remove("hidden")
+  inputElement.classList.remove("hidden")
+  
+
+  carElement.style.left='0';
+
+  // get a quote
+  const quoteIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[quoteIndex];
+  // Put the quote into an array of words
+  words = quote.split(' ');
+  // reset the word index for tracking
+  wordIndex = 0;
+
+  // UI updates
+  // Create an array of span elements so we can set a class
+  const spanWords = words.map(function (word) { return `<span>${word} </span>` });
+  // Convert into string and set as innerHTML on quote display
+  quoteElement.innerHTML = spanWords.join('');
+  // Highlight the first word
+  quoteElement.childNodes[0].className = 'highlight';
+  // Clear any prior messages
+  messageElement.innerText = '';
+
+  // Setup the textbox
+  // Clear the textbox
+  typedValueElement.value = '';
+  // set focus
+  typedValueElement.focus();
+  // set the event handler
+
+  // Start the timer
+  startTime = new Date().getTime();
+});
+
+// at the end of script.js
 typedValueElement.addEventListener('input', () => {
-    // Get the current word
-    const currentWord = words[wordIndex];
-    // get the current value
-    const typedValue = typedValueElement.value;
-  
-    if (typedValue === currentWord && wordIndex === words.length - 1) {
-      // end of sentence
-      // Display success
-      const elapsedTime = new Date().getTime() - startTime;
-      const message = `You did it in ${elapsedTime / 1000} seconds.`;
-      alert(message)
-    } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
-      // end of word
-      // clear the typedValueElement for the new word
-      typedValueElement.value = '';
-      // move to the next word
-      wordIndex++;
-      // reset the class name for all elements in quote
-      for (const wordElement of quoteElement.childNodes) {
-        wordElement.className = '';
-      }
-      // highlight the new word
-      quoteElement.childNodes[wordIndex].className = 'highlight';
-    } else if (currentWord.startsWith(typedValue)) {
-      // currently correct
-      // highlight the next word
-      typedValueElement.className = '';
-    } else {
-      // error state
-      typedValueElement.className = 'error';
+  // Get the current word
+  const currentWord = words[wordIndex];
+
+  // get the current value
+  const typedValue = typedValueElement.value;
+
+  if (typedValue === currentWord && wordIndex === words.length - 1) {
+    // end of sentence
+    // Display success
+    typedValueElement.classList.add("hidden")
+    quoteElement.classList.add("hidden")
+    clickToGameElement.classList.remove('hidden')
+    const newClickToText = 'Click <span class="pink">TRY AGAIN</span> to restart!';
+
+    clickToGameElement.innerHTML = newClickToText;
+    const newButtonText = 'Try again';
+    startButtonElement.classList.remove("hidden")
+    startButtonElement.innerText = newButtonText;
+    
+    const elapsedTime = new Date().getTime() - startTime;
+    const message = `You did it in ${elapsedTime / 1000} seconds.`;
+    alert(message)
+  } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+    // end of word
+    // clear the typedValueElement for the new word
+    typedValueElement.value = '';
+    // move to the next word
+    wordIndex++;
+    // reset the class name for all elements in quote
+    for (const wordElement of quoteElement.childNodes) {
+      wordElement.className = '';
     }
-  });
+    // highlight the new word
+    quoteElement.childNodes[wordIndex].className = 'highlight';
+
+    const progress = wordIndex* 100 / words.length;
+    carElement.style.left = `${progress}%`;
+  } else if (currentWord.startsWith(typedValue)) {
+    // currently correct
+    // highlight the next word
+    typedValueElement.className = '';
+  } else {
+    // error state
+    typedValueElement.className = 'error';
+  }
+});
